2f91c71ef76787da83c44407cd293650ba4a0bbd
Fayland Lam <fayland@gmail.com>
2015-12-10 16:21:38 +0800

    Add BIAS

--- miner/index_history_data.pl
+++ miner/index_history_data.pl
@@ -10,6 +10,7 @@ use Encode;
 use JSON;
 use Data::Dumper;
 use Template;
+use List::Util qw/sum/;
 
 my $dbh = dbh();
 
@@ -23,6 +24,13 @@ while (my ($s, $n) = $sth->fetchrow_array) {
     my $history_sth = $dbh->prepare("SELECT * FROM stock_history WHERE symbol = ? ORDER BY date DESC LIMIT 91");
     $history_sth->execute($s);
     while (my $h = $history_sth->fetchrow_hashref) { push @history, $h }
+
+    ## M25 均线
+    my @h25 = @history[0..24];
+    @h25 = map { $_->{close} } @h25;
+    my $total = sum @h25;
+    my $ma25  = $total / 25;
+    $row->{BIAS25} = int(($history[0]->{close} - $ma25) * 10000 / $ma25) / 100;
 
     $row->{history} = \@history;
     push @rows, $row;
