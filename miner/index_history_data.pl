5c8810074a7627aa4f925bee4f1c90b99c0716f8
Fayland Lam <fayland@gmail.com>
2015-12-29 16:44:45 +0800

    xx

--- miner/index_history_data.pl
+++ miner/index_history_data.pl
@@ -32,6 +32,23 @@ while (my ($s, $n) = $sth->fetchrow_array) {
     my $ma25  = $total / 25;
     $row->{BIAS25} = int(($history[0]->{close} - $ma25) * 10000 / $ma25) / 100;
 
+    my @h30 = @history[0..29];
+    @h30 = map { $_->{close} } @h30;
+    $total = sum @h30;
+    my $ma30 = $total / 30;
+    # $row->{BIAS30} = int(($history[0]->{close} - $ma30) * 10000 / $ma30) / 100;
+
+    my @h5 = @history[0..4];
+    @h5 = map { $_->{close} } @h5;
+    $total = sum @h5;
+    my $ma5 = $total / 5;
+    # my @h60 = @history[0..59];
+    # @h60 = map { $_->{close} } @h60;
+    # $total = sum @h60;
+    # my $ma60 = int(100 * $total / 60) / 100;
+
+    $row->{MA5_30} = int(($ma5 - $ma30) * 10000 / $ma30) / 100;
+
     $row->{history} = \@history;
     push @rows, $row;
 }
