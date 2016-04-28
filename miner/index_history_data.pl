0d31f9b270f5b8afa27b70d6f6f1a9e139e82c93
Fayland Lam <fayland@gmail.com>
2016-04-28 21:30:55 +0800

    Add BIAS20

--- miner/index_history_data.pl
+++ miner/index_history_data.pl
@@ -33,22 +33,28 @@ while (my ($s, $n) = $sth->fetchrow_array) {
     my $ma25  = $total / 25;
     $row->{BIAS25} = int(($history[0]->{close} - $ma25) * 10000 / $ma25) / 100;
 
-    my @h30 = @history[0..29];
-    @h30 = map { $_->{close} } @h30;
-    $total = sum @h30;
-    my $ma30 = $total / 30;
-    # $row->{BIAS30} = int(($history[0]->{close} - $ma30) * 10000 / $ma30) / 100;
+    my @h20 = @history[0..19];
+    @h20 = map { $_->{close} } @h20;
+    $total = sum @h20;
+    my $ma20  = $total / 20;
+    $row->{BIAS20} = int(($history[0]->{close} - $ma20) * 10000 / $ma20) / 100;
 
-    my @h5 = @history[0..4];
-    @h5 = map { $_->{close} } @h5;
-    $total = sum @h5;
-    my $ma5 = $total / 5;
-    # my @h60 = @history[0..59];
-    # @h60 = map { $_->{close} } @h60;
-    # $total = sum @h60;
-    # my $ma60 = int(100 * $total / 60) / 100;
+    # my @h30 = @history[0..29];
+    # @h30 = map { $_->{close} } @h30;
+    # $total = sum @h30;
+    # my $ma30 = $total / 30;
+    # # $row->{BIAS30} = int(($history[0]->{close} - $ma30) * 10000 / $ma30) / 100;
 
-    $row->{MA5_30} = int(($ma5 - $ma30) * 10000 / $ma30) / 100;
+    # my @h5 = @history[0..4];
+    # @h5 = map { $_->{close} } @h5;
+    # $total = sum @h5;
+    # my $ma5 = $total / 5;
+    # # my @h60 = @history[0..59];
+    # # @h60 = map { $_->{close} } @h60;
+    # # $total = sum @h60;
+    # # my $ma60 = int(100 * $total / 60) / 100;
+
+    # $row->{MA5_30} = int(($ma5 - $ma30) * 10000 / $ma30) / 100;
 
     $row->{history} = \@history;
     push @rows, $row;
